cb4f7002bdaf530501edebed6a45c62231112f1b
Saud M. Khoja <65879129+saudmjd@users.noreply.github.com>
2020-07-30 19:12:51 -0400

    JS

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $("#getWeather").click(function(){
+        let location = $(".location").val(); 
+        
+        if(location != ''){
+            $.ajax({
+                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + location + "&units=metric" + "&APPID=ef7c53304d696d0f76cc01d25b00fd8d",
+                type: "GET",
+                dataType: "json", //tested. no need for 'jsonp'
+                success: function(data){
+                    let weatherApp = show(data);
+                    $(".temperature").html(weatherApp);
+                }
+            })
+        }else{
+            $(".err").html('Opps, please enter a valid location')
+        }
+        
+    });
+});
+
+//this function will display what is in the success callback function. So in this case, all the data. 
+function show(data){  
+    return "<p class='temp'>" + data.main.temp +" C°</p>";
+}
